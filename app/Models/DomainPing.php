0fbbcb7cd06bdf195539b9b4d0b159604fc4f798
romainvausetrustup <romain.vause@trustup.be>
2022-07-14 11:38:25 +0200

    Catch all exception for a fialed job (failed ping)

--- app/Models/DomainPing.php
+++ app/Models/DomainPing.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class DomainPing extends Model
 {
 
-    protected $fillable = ['url', 'status', 'answer_time_ms', 'domain_ping_batch_id', 'dns_a'];
+    protected $fillable = ['url', 'status', 'status_reason', 'answer_time_ms', 'domain_ping_batch_id', 'dns_a'];
 
     protected $casts = [
         'batch_datetime' => 'datetime'
