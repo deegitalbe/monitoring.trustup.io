f8950d6ca6f1527fadca7058d52905afeacd8c25
romainvausetrustup <romain.vause@trustup.be>
2022-07-14 10:11:29 +0200

    Show tooltip

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import './bootstrap';
+import 'tw-elements'
 
 import Alpine from 'alpinejs';
-import nextTick from "alpinejs";
 
 window.Alpine = Alpine;
 Alpine.start();
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -32,7 +32,16 @@
             {{ $slot }}
         </div>
 
-
+        <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
+        <script src="https://unpkg.com/@popperjs/core@2.9.1/dist/umd/popper.min.js" charset="utf-8"></script>
+        <script type="text/javascript">
+            var tooltipTriggerList = [].slice.call(
+                document.querySelectorAll('[data-bs-toggle="tooltip"]')
+            );
+            var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+                return new Tooltip(tooltipTriggerEl);
+            });
+        </script>
         @vite(['resources/js/app.js'])
     </body>
 </html>
